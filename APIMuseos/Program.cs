c2bc6a3dceba1fe97072e624aac56f4c4b39bd17
JorgeMercadoF <47115493+JorgeMercadoF@users.noreply.github.com>
2019-02-06 19:28:46 +0100

    cambio1

--- APIMuseos/Program.cs
+++ APIMuseos/Program.cs
@@ -1,15 +1,65 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace APIMuseos
 {
+    class Museo /*Contenido de la API https://museowebapp.azurewebsites.net/api/MuseosAPI */
+    {
+        public int Id { get; set; }
+        public string Coordenadas { get; set; }
+        public string Datacion { get; set; }
+        public string Descripcion { get; set; }
+        public string Titulo { get; set; }
+        public string Imagen { get; set; }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static async void leeMuseos()
         {
+            List<Museo> ListaMuseos = new List<Museo>();
+            HttpClient Clientes = new HttpClient();
+            string url = @"https://museowebapp.azurewebsites.net/api/MuseosAPI";
+            var uri = new Uri(url);
+
+            var response = await Clientes.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                ListaMuseos = JsonConvert.DeserializeObject<List<Museo>>(content);
+
+                foreach(var mus in ListaMuseos)
+                {
+                    Console.WriteLine(mus.Descripcion);
+                }
+            }
+        }
+
+        static async void leeMuseosVisitas()
+        {
+            HttpClient Clientes = new HttpClient();
+            string url = @"https://museowebapp.azurewebsites.net/api/MuseosAPI";
+            var uri = new Uri(url);
+
+            var response = await Clientes.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                JArray Array = JArray.Parse(content);
+                var visitas = from museo in Array where (campo => campo["visitas"].Equals("S"));
+            }
+        }
+
+        static void Main (string[] args)
+        {
+            leeMuseos();
+            Console.ReadLine();
         }
     }
 }
